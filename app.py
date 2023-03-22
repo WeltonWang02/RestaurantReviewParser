11620407d0773a562e7106cb8e6c3014de3534e1
Welton Wang <weltonwang88@gmail.com>
2023-03-22 16:34:41 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from gensim.models import KeyedVectors
+from sanic import Sanic
+from sanic import response
+from sanic.exceptions import SanicException, ServerError, NotFound
+from reviews import *
+import os
+
+model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+
+class FailedHTTPResponse(Exception):
+    "Raised when zembra returns an failed response"
+    pass
+
+app = Sanic(__name__)
+@app.route("/", methods=['POST', 'GET'])
+async def easter(request):
+    return response.text('{"response":"Invalid endpoint"}')
+  
+@app.route("/api/v1/fetch/", methods=['POST', 'GET'])
+async def analysis(request):
+    if request.method == 'POST':
+        request_json = request.json
+        pred = Restaurant(request_json['location'], model)
+        pred.fetch_reviews(int(os.environ['REVIEW_COUNT']))
+        pred.process_review_data()
+        res = pred.return_rankings()
+        return response.json({'response':"Success", "list":res})
+    else:
+        return response.json({"response":"Invalid response method"})
+
+@app.exception(NotFound)
+async def manage_not_found(request, exception):
+    return response.text('{"response":"Invalid endpoint"}')
+
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=os.environ['PORT'],
+    )
